5dbc936afe0df4c01f01de0abd580b5687b60722
DELL <Swetha>
2024-03-01 01:44:08 +0530

    Add project files.

---
+++ BlogTrackAPI/Data/BlogerDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlogTrackAPI.Models;
+
+namespace BlogTrackAPI.Data
+{
+    public class BlogerDbContext : DbContext
+    {
+        public BlogerDbContext (DbContextOptions<BlogerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlogTrackAPI.Models.AdminInfo> AdminInfo { get; set; } = default!;
+
+        public DbSet<BlogTrackAPI.Models.BlogInfo>? BlogInfo { get; set; }
+
+        public DbSet<BlogTrackAPI.Models.EmpInfo>? EmpInfo { get; set; }
+    }
+}
---
+++ BlogWebAppMVC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using BlogTrackAPI.Data;
+using BlogWebAppMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogWebAppMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly BlogerDbContext _context;
+        public HomeController(ILogger<HomeController> logger, BlogerDbContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var blogs = await _context.BlogInfo.ToListAsync();
+            return View(blogs);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
